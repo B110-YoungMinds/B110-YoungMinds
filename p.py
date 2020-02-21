3e9bdbb99c519ddb30368c697911a565e833aa41
amrishkumar <amrishkumarg2000@gmail.com>
2020-02-21 15:34:26 +0530

    VOICE`

---
+++ p.py
@@ -0,0 +1,76 @@
+import speech_recognition as sr
+import importlib
+def rec1():
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:                                                                       
+        print("Speak:p")
+        audio = r.record(source,duration=5)   
+
+    try:
+        message=r.recognize_google(audio)
+        print(message.split())
+        mess=message.split()
+        for i in range(len(mess)):
+            if(mess[i]=="open"):
+                if(mess[i+1]=="YouTube" or mess[i+1]=="youtube" or mess[i+1]=="gmail" or mess[i+1]=="Gmail" ):
+                    full_module_name = "webopener"
+                    akrun = importlib.import_module(full_module_name)
+                    akrun.webopener(mess[i+1],"")
+                elif(mess[i+1][-4:]==".com"):
+                    full_module_name = "webopener"
+                    akrun = importlib.import_module(full_module_name)
+                    akrun.webopener(mess[i+1],"s")
+                else:
+                    full_module_name = "app_opener"
+                    akrun = importlib.import_module(full_module_name)
+                    if(len(mess)==2):
+                        akrun.openapp(mess[i+1])
+                    elif(mess[i+2]=="for" or mess[i+2]=="For"):
+                        akrun.openapp(mess[i+1])
+                    else:
+                        akrun.openapp(mess[i+1]+" "+mess[i+2])
+                    break
+            elif(mess[i]=="search" or mess[i]=="Search"):
+                work=""
+                for j in range(len(mess)):
+                    if(mess[j]=="in"):
+                        if(mess[j+1]=="YouTube" or mess[j+1]=="youtube" or mess[j+1]=="chrome" or mess[j+1]=="Chrome"):
+                            full_module_name = "webopener"
+                            akrun = importlib.import_module(full_module_name)
+                            work=work.split()
+                            s=" "
+                            s = s.join(work[1:])
+                            akrun.webopener(mess[j+1],s)
+                    else:
+                        if(mess[j]!="Search" or mess[j]!="search"):
+                            work=work+" "+mess[j]
+            elif(mess[i]=="play" or mess[i]=="Play"):
+                work=""
+                for j in range(len(mess)):
+                    if(mess[j]=="in"):
+                        if(mess[j+1]=="YouTube" or mess[j+1]=="youtube"):
+                            full_module_name = "webopener"
+                            akrun = importlib.import_module(full_module_name)
+                            work=work.split()
+                            s=" "
+                            s = s.join(work[1:])
+                            akrun.webopener(mess[j+1],s)
+                    else:
+                        if("in" in mess):
+                            if(mess[j]!="Search" or mess[j]!="search"):
+                                work=work+" "+mess[j]
+                        else:
+                            full_module_name = "webopener"
+                            akrun = importlib.import_module(full_module_name)
+                            s=" "
+                            s = s.join(mess[1:])
+                            akrun.webopener("YouTube",s)
+            elif(mess[i]=="mail" or mess[i]=="Mail" or mess[i]=="Gmail" or mess[i]=="gmail" or mess[i]=="email" or mess[i]=="Email"):
+                full_module_name = "webopener"
+                akrun = importlib.import_module(full_module_name)
+                akrun.webopener("gmail","")
+    except:
+        pass
+i=-1
+while(i==-1):
+    rec1()
---
+++ webopener.py
@@ -0,0 +1,42 @@
+import time
+import webbrowser
+import importlib
+import pyttsx3
+
+def webopener(app,work):
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    if(work=="s"):
+        webbrowser.get(chrome_path).open(app)
+    elif(work==""):
+        app=app+".com"
+        webbrowser.get(chrome_path).open(app)
+        if(app=="YouTube.com"):
+            full_module_name = "youtube_control"
+            akrun = importlib.import_module(full_module_name)
+            engine = pyttsx3.init()
+            rate = engine.getProperty('rate')            
+            engine.setProperty('rate', 150)
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[1].id)
+            engine.say("Tell the number of the link")
+            engine.say("I repeat Tell the number of the link")
+            engine.runAndWait()
+            akrun.startcont()
+        else:
+            print("email")
+    else:
+        if(app=="YouTube"):
+            webbrowser.get(chrome_path).open("https://www.youtube.com/results?search_query="+work)
+            full_module_name = "youtube_control"
+            akrun = importlib.import_module(full_module_name)
+            engine = pyttsx3.init()
+            rate = engine.getProperty('rate')            
+            engine.setProperty('rate', 150)
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[1].id)
+            engine.say("Tell the number of the link")
+            engine.say("I repeat Tell the number of the link")
+            engine.runAndWait()
+            akrun.startcont()
+        elif(app=="Chrome" or app=="chrome"):
+            webbrowser.get(chrome_path).open("https://www.google.com/search?q="+work)
